663c4036b86606f2db3cad7dff39fff2f4365cfe
Renan Soares <renansoaresffc@gmail.com>
2022-05-02 20:09:50 -0300

    Navbar component

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,52 @@
+import Logo from 'assets/img/logo.png';
+
+import 'bootstrap/js/src/collapse.js';
+import './styles.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light pt-4 pb-4 main-nav">
+            <div className="container">
+                <a href="/" className="nav-logo-text">
+                    <img src={Logo} alt="Learn Logo" />
+                </a>
+
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#learn-navbar"
+                    aria-controls="learn-navbar"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div id="learn-navbar" className="collapse navbar-collapse nav-items">
+                    <ul className="navbar-nav main-menu">
+                        <li>
+                            <a href="/" className="active">Home</a>
+                        </li>
+                        <li>
+                            <a href="/produtos">Cursos</a>
+                        </li>
+                        <li>
+                            <a href="/eventos">Eventos</a>
+                        </li>
+                        <li>
+                            <a href="/recursos">Recursos</a>
+                        </li>
+                    </ul>
+                    
+                    <div className="group-buttons">
+                        <a href="#login" role="button" className="btn-login">Login</a>
+                        <a href="#register" role="button" className="btn-register">Cadastre-se</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
