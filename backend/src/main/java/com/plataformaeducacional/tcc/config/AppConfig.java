9094d270f0893149539ed1cc52d158cb86711f91
Renan Soares <renansoaresffc@gmail.com>
2022-05-13 18:53:06 -0300

    Implementing OAuth2 authorization server

--- backend/src/main/java/com/plataformaeducacional/tcc/config/AppConfig.java
+++ backend/src/main/java/com/plataformaeducacional/tcc/config/AppConfig.java
@@ -3,6 +3,8 @@ package com.plataformaeducacional.tcc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +12,17 @@ public class AppConfig {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-JWT-SECRET");		// Registra a chave do token
+		return tokenConverter;
+	}
+
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
 	}
 }
