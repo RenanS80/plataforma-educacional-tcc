8bde2d1256267bb7a8003ef238b4d2768ad781de
Renan Soares <renansoaresffc@gmail.com>
2022-05-20 21:37:11 -0300

    React Hook Form to CourseFilter, CourseFilter layout responsive

--- frontend/src/components/CourseFilter/index.tsx
+++ frontend/src/components/CourseFilter/index.tsx
@@ -2,32 +2,73 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 import './styles.css';
+import { Controller, useForm } from 'react-hook-form';
+import { Category } from 'types/Category';
+import Select from 'react-select';
+import { useEffect, useState } from 'react';
+import { requestBackend } from 'utils/requests';
+
+type CourseFilterData = {
+    name: string,
+    category: Category;
+}
 
 function CourseFilter() {
 
-    return (
-        <div className="base-card course-filter-container">
-            <form action="" className="course-filter-form">
-                <div className="course-filter-name">
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Pesquisar curso" 
-                    />
-                    <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" />
-                </div>
+    const { register, handleSubmit, control } = useForm<CourseFilterData>();
 
-                <div className="course-filter-bottom">
-                    <div className="course-filter-category">
-                        <select name="" id="">
-                            <option value="">Front-end</option>
-                        </select>
-                    </div>
-                    <button className="btn-clean-filter">Limpar filtro</button>
+    const [selectCategories, setSelectCategories] = useState<Category[]>([]);
+
+    useEffect(() => {
+        requestBackend({ url: `/categories` })
+            .then((response) => {
+                setSelectCategories(response.data.content);
+            })
+    }, [])
+
+            const onSubmit = (formData: CourseFilterData) => {
+                console.log('ENVIOU', formData)
+            }
+
+            return (
+                <div className="base-card course-filter-container">
+                    <form onSubmit={handleSubmit(onSubmit)} className="course-filter-form">
+                        <div className="course-filter-name-container">
+                            <input
+                                {...register('name')}
+                                type="text"
+                                className="form-control"
+                                placeholder="Pesquisar curso"
+                                name="name"
+                            />
+                            <button className="course-filter-button">
+                                <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" />
+                            </button>
+                        </div>
+
+                        <div className="course-filter-bottom-container">
+                            <div className="course-filter-category-container">
+                                <Controller
+                                    name="category"
+                                    control={control}
+                                    render={({ field }) => (
+                                        <Select
+                                            {...field}
+                                            options={selectCategories}
+                                            classNamePrefix="course-filter-select"
+                                            isClearable
+                                            getOptionLabel={(category: Category) => category.name}
+                                            getOptionValue={(category: Category) => String(category.id)}
+                                            placeholder="Categoria"
+                                        />
+                                    )}
+                                />
+                            </div>
+                            <button className="btn-clear-filter">Limpar<span className="btn-clear-filter-word"> Filtro</span></button>
+                        </div>
+                    </form>
                 </div>
-            </form>
-        </div>
-    );
-}
+            );
+        }
 
 export default CourseFilter;
